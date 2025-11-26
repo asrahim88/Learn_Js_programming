679b9c3d58f6724e8268d22f4e28763f4020c336
asrahim88 <asrahim162@gmail.com>
2025-11-26 20:56:39 +0600

    practice variable

--- p_1_program.js
+++ p_1_program.js
@@ -1,1 +1,31 @@
-console.log("Hello World!");
+var number1 = 10;
+var number2 = 10.5;
+var number3 = '10';
+var number4 = '10.5';
+var text = 'hello';
+var text1 = "Hello world";
+var text2 =  `Hello Javascript`;
+
+
+console.log(number1, number2, number3, number4, text, text1, text2);
+
+console.log(`number1 = ${number1}`,typeof number1);
+console.log(`number2= ${number2}`,typeof number2);
+console.log(`number3= ${number3}`,typeof number3);
+console.log(`number4= ${number4}`,typeof number4);
+console.log(`text= ${text}`,typeof text);
+console.log(`text1= ${text1}`,typeof text1);
+console.log(`text2= ${text2}`,typeof text2);
+
+var number3 = parseInt(number3);
+var number5 = parseFloat(number3);
+
+console.log(typeof number3);
+console.log(typeof number5);
+
+var num = 10.585467892;
+console.log(num);
+
+console.log(num.toFixed(2));var text = "hello";
+// let name = prompt("Enter your name: ");
+console.log(text);
