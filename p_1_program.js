e0c8aaf4ea8a19d4de55d064ebbda6c2586707d8
asrahim88 <asrahim162@gmail.com>
2026-06-25 17:55:34 +0600

    js practice 1st day with variable

--- p_1_program.js
+++ p_1_program.js
@@ -1,34 +1,58 @@
-var number1 = 10;
+var number1 = 10
+console.log(number1);
 var number2 = 10.5;
-var number3 = '10';
-var number4 = '10.5';
-var text = 'hello';
-var text1 = "Hello world";
-var text2 =  `Hello Javascript`;
-
-
-console.log(number1, number2, number3, number4, text, text1, text2);
-
-console.log(`number1 = ${number1}`,typeof number1);
-console.log(`number2= ${number2}`,typeof number2);
-console.log(`number3= ${number3}`,typeof number3);
-console.log(`number4= ${number4}`,typeof number4);
-console.log(`text= ${text}`,typeof text);
-console.log(`text1= ${text1}`,typeof text1);
-console.log(`text2= ${text2}`,typeof text2);
-
-var number3 = parseInt(number3);
-var number5 = parseFloat(number3);
+console.log(number2);
+var number3 = '10.5';
+console.log(number3);
+var number4 = 'hello world';
+console.log(number4);
 
 console.log(typeof number3);
-console.log(typeof number5);
+console.log(typeof number4);
+var text = `Bismillahir Rahmanir Rahim`;
+var text1 = "Bismillahir Rahmanir Rahim";
+var text2 = 'Bismillahir Rahmanir Rahim';
+console.log(text, '\n', text1, '\n', text2);
 
-var num = 10.585467892;
-console.log(num);
+console.log(`Number1 = ${number1}\nNumber2 = ${number2}\nTotal Sum = ${number1 + number2}`);
 
-console.log(num.toFixed(2));var text = "hello";
+var isReach = true;
+console.log(typeof isReach);
+var passed = false;
+console.log(typeof passed);
+console.log(`isReach = ${isReach}\npassed = ${passed}`);
 
-console.log(text);
+// var number1 = 10;
+// var number2 = 10.5;
+// var number3 = '10';
+// var number4 = '10.5';
+// var text = 'hello';
+// var text1 = "Hello world";
+// var text2 =  `Hello Javascript`;
 
-var isTrue = true;
-console.log(typeof isTrue);
+
+// console.log(number1, number2,  number3, number4, text, text1, text2);
+
+// console.log(`number1 = ${number1}`,typeof number1);
+// console.log(`number2= ${number2}`,typeof number2);
+// console.log(`number3= ${number3}`,typeof number3);
+// console.log(`number4= ${number4}`,typeof number4);
+// console.log(`text= ${text}`,typeof text);
+// console.log(`text1= ${text1}`,typeof text1);
+// console.log(`text2= ${text2}`,typeof text2);
+
+// var number3 = parseInt(number3);
+// var number5 = parseFloat(number3);
+
+// console.log(typeof number3);
+// console.log(typeof number5);
+
+// var num = 10.585467892;
+// console.log(num);
+
+// console.log(num.toFixed(2));var text = "hello";
+
+// console.log(text);
+
+// var isTrue = true;
+// console.log(typeof isTrue);
